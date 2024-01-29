7299088794193c30a34ea93d08a9eed72076517f
Nicolas <nicolas.pp333@gmail.com>
2024-01-29 14:10:17 -0300

    Class 150

--- ManejoPresupuesto/Controllers/CuentasController.cs
+++ ManejoPresupuesto/Controllers/CuentasController.cs
@@ -91,6 +91,11 @@ namespace ManejoPresupuesto.Controllers
             modelo.FechaInicio = fechaInicio;
             modelo.FechaFin = fechaFin;
 
+            ViewBag.mesAnterior = fechaInicio.AddMonths(-1).Month;
+            ViewBag.anioAnterior = fechaInicio.AddMonths(-1).Year;
+            ViewBag.mesPosterior = fechaInicio.AddMonths(1).Month;
+            ViewBag.anioPosterior = fechaInicio.AddMonths(1).Year;
+
             return View(modelo);
         }
 
